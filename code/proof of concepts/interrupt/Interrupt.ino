7bda1d1be159959db2dbd76a14fc34ef021b8a19
kamielhoskens <146442947+kamielhoskens@users.noreply.github.com>
2023-12-19 13:43:04 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/Interrupt.ino
@@ -0,0 +1,63 @@
+
+int led = 13;
+bool startprogram = false;
+bool stopprogram = false;
+int i = 0;
+
+
+void setup() 
+{
+
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(7, INPUT); //als we pulldownweerstand willen weglaten -> softwarematige INPUT_PULLDOWN
+  attachInterrupt(4, ISR_test, RISING); // attachInterrupt(interuptnr van pin 4, Interupt Service Routine, overgang van 0 naar 1);//
+
+}
+
+void loop() 
+{
+  if(startprogram == true)
+  {
+    digitalWrite(led,HIGH);
+    
+  }
+
+  if (stopprogram == true)
+  {
+    digitalWrite(led,LOW);
+  }
+
+}
+
+void ISR_test(void)
+{
+  i++;
+  if (i == 1)
+  {
+    startprogram = true;
+    stopprogram = false;
+    Serial.println("Programma start");
+
+  }
+
+  else if (i == 2)
+  {
+    stopprogram = true;
+    startprogram = false;
+    Serial.println("Programma stopt");
+    i = 0;
+  }
+  
+  else
+  {
+
+  }
+  
+}
+  
+
+  
+
+
+
